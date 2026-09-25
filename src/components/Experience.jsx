e714f6b555700d36ccfad500bdfac02154badcfd
Sreeja Reddy Yeluru <72263845+yelurusreejareddy@users.noreply.github.com>
2026-09-24 20:03:17 -0500

    Use Application Development Analyst title

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,7 +13,7 @@ const roles = [
     ],
   },
   {
-    title: 'Application Development Associate to Analyst',
+    title: 'Application Development Analyst',
     org: 'Accenture',
     period: 'Oct 2021 to Aug 2024',
     location: 'Bangalore, India',
